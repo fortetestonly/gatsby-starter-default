ca930b516e192c7332cb2dba47361fe04c8349df
fortetestonly <43376028+fortetestonly@users.noreply.github.com>
2019-07-26 13:00:43 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,24 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+      
     },
+    {
+  resolve: 'gatsby-source-wordpress',
+  options: {
+    // The base url to your WP site.
+    baseUrl: 'techsevenpartners.com',
+    // WP.com sites set to true, WP.org set to false
+    hostingWPCOM: false,
+    // The protocol. This can be http or https.
+    protocol: 'https',
+    // Use 'Advanced Custom Fields' Wordpress plugin
+    useACF: true,
+    auth: {},
+    // Set to true to debug endpoints on 'gatsby build'
+    verboseOutput: false
+  }
+},
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
